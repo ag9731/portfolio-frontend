bc0f488f11a2127727ad64920662d1a965b180ee
Akash <goravanakollakash@gmail.com>
2025-11-20 01:41:48 +0530

    changes made in lenovo

--- src/Axios.jsx
+++ src/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://portfolio-backend-0qew.onrender.com/",
   timeout: 5000,
   withCredentials: true,
   headers: { "X-Custom-Header": "foobar" },
